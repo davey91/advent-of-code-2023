2b94436fc9abf07edef46715d5fac33b97b4e924
daveybrouwer <>
2023-12-08 14:01:22 +0100

    day4 - part 2

--- src/main/scala/com/brouwer/aoc/main/day4/A.scala
+++ src/main/scala/com/brouwer/aoc/main/day4/A.scala
@@ -11,6 +11,13 @@ case class Card(number: Int, winningNumbers: List[Int], cardNumbers: List[Int]) 
     case Nil => 0
     case notEmpty => notEmpty.tail.foldLeft(1)((total, _) => total * 2)
   }
+
+  def findWinningCards(cards: Seq[Card]): Int = {
+    totalWin match {
+      case Nil => 1
+      case nonEmpty => nonEmpty.zipWithIndex.map{ case (_, index) => cards(number+index).findWinningCards(cards)}.sum + 1
+    }
+  }
 }
 
 trait SimpleParser extends RegexParsers {
@@ -18,21 +25,25 @@ trait SimpleParser extends RegexParsers {
   def number: Parser[Int] = """(0|[1-9]\d*)""".r ^^ { _.toInt }
   def semicolon: Parser[String] = """:""".r ^^ { _.toString }
   def winningNumbersSeperator: Parser[String] = """\|""".r ^^ { _.toString }
-  def cardWithGames: Parser[Card] = card ~ number ~ semicolon ~ rep(number) ~ winningNumbersSeperator ~ rep(number) ^^ { case cd ~ nr ~ _ ~ winningNumbers ~ _ ~ cardNumbers => Card(nr, winningNumbers, cardNumbers) }
+  def cardWithGames: Parser[Card] = card ~ number ~ semicolon ~ rep(number) ~ winningNumbersSeperator ~ rep(number) ^^ { case _ ~ nr ~ _ ~ winningNumbers ~ _ ~ cardNumbers => Card(nr, winningNumbers, cardNumbers) }
 }
 
 object A extends App with SimpleParser {
 
   val file = Source.fromResource("day4-1.txt")
 
-  val r = file
+  val cards = file
     .getLines()
     .toSeq
     .map(parse(cardWithGames, _))
     .map(_.get)
 
-  val solutionA = r.map(_.totalPoints).sum
+  val solutionA = cards.map(_.totalPoints).sum
+
+  val solutionB = cards.map(_.findWinningCards(cards)).sum
 
   println(solutionA)
+
+  println(solutionB)
 
 }
